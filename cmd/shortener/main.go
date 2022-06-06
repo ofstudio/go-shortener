b31605f122293bf836d1322d5afeffdfdd61d873
Oleg Fomin <ofstudio@gmail.com>
2022-06-06 22:27:06 +0300

    update main

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/ofstudio/go-shortener/internal/app"
+	"github.com/ofstudio/go-shortener/internal/app/config"
+	"github.com/ofstudio/go-shortener/internal/app/services"
 	"github.com/ofstudio/go-shortener/internal/handlers"
 	"github.com/ofstudio/go-shortener/internal/storage"
 	"log"
@@ -11,12 +12,17 @@ import (
 )
 
 func main() {
-	cfg := app.NewConfig(4096, "http://localhost:8080/")
-	a := app.NewApp(cfg, storage.NewMemory())
+	cfg := &config.Config{
+		UrlMaxLen: 4096,
+		PublicURL: "http://localhost:8080/",
+	}
+	db := storage.NewMemoryStorage()
+	srv := services.NewShortenerService(cfg, db)
+	h := handlers.NewShortenerHandlers(srv)
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Mount("/", handlers.NewURLsResource(a).Routes())
+	r.Mount("/", h.Routes())
 
 	server := &http.Server{
 		Addr:    "localhost:8080",
