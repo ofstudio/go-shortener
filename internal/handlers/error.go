3c6c44985f064dcafcbe5fcd3ed6c746dcb99f73
Oleg Fomin <ofstudio@gmail.com>
2022-06-10 18:55:04 +0300

    refactor respond with error function

--- internal/handlers/error.go
+++ internal/handlers/error.go
@@ -6,12 +6,15 @@ import (
 	"net/http"
 )
 
-// errorResponse - возвращает клиенту http-ошибку, соотвествующую ошибке сервиса
-func errorResponse(w http.ResponseWriter, err error) {
+var ErrValidation = errors.New("validation error")
+
+// respondWithError - возвращает клиенту http-ошибку, соотвествующую ошибке сервиса
+func respondWithError(w http.ResponseWriter, err error) {
 	switch {
 	case errors.Is(err, services.ErrShortURLNotFound):
 		http.Error(w, "Not found", http.StatusNotFound)
-	case errors.Is(err, services.ErrValidation):
+	case errors.Is(err, services.ErrValidation),
+		errors.Is(err, ErrValidation):
 		http.Error(w, "Bad request", http.StatusBadRequest)
 	default:
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
