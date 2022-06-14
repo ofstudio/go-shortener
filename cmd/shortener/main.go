e3aa656535b4f071101d78009491b84edd477a84
Oleg Fomin <ofstudio@gmail.com>
2022-06-14 22:04:26 +0300

    enable compressor middleware

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"compress/gzip"
 	"github.com/go-chi/chi/v5"
 	chiMiddleware "github.com/go-chi/chi/v5/middleware"
 	"github.com/ofstudio/go-shortener/internal/app/config"
@@ -51,7 +52,10 @@ func main() {
 	// Middleware для компрессии ответов.
 	// Параметр minSize рекомендуется равным middleware.MTUSize.
 	// Значение 0 означает сжатие ответов любой длины и используется в целях демонстрации.
-	//r.Use(middleware.NewCompressor(0, gzip.BestSpeed).AddType("application/json").Handler)
+	r.Use(middleware.NewCompressor(0, gzip.BestSpeed).
+		AddType("application/json").
+		AddType("text/plain").
+		AddType("text/html").Handler)
 
 	// Добавляем маршруты для обработки запросов.
 	r.Mount("/", appHandlers.Routes())
