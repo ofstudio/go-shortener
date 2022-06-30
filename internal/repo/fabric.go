190c13f7753241bcb76b38ac7ff6942ccf37d61c
Oleg Fomin <ofstudio@gmail.com>
2022-06-30 20:59:00 +0300

    add selected repo type logging

--- internal/repo/fabric.go
+++ internal/repo/fabric.go
@@ -1,6 +1,9 @@
 package repo
 
-import "github.com/ofstudio/go-shortener/internal/app/config"
+import (
+	"github.com/ofstudio/go-shortener/internal/app/config"
+	"log"
+)
 
 // Fabric - фабрика для создания репозитория.
 // Если задан DatabaseDSN - используем SQL-репозиторий.
@@ -9,10 +12,13 @@ import "github.com/ofstudio/go-shortener/internal/app/config"
 func Fabric(cfg *config.Config) Repo {
 	switch {
 	case cfg.DatabaseDSN != "":
+		log.Print("Using Postgres storage")
 		return MustNewSQLRepo(cfg.DatabaseDSN)
 	case cfg.FileStoragePath != "":
+		log.Print("Using file storage")
 		return MustNewAOFRepo(cfg.FileStoragePath)
 	default:
+		log.Print("Using in-memory storage")
 		return NewMemoryRepo()
 	}
 }
