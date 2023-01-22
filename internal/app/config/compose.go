53c485b689330307de3ff7e6cbf12491b22f8333
Oleg Fomin <ofstudio@gmail.com>
2023-01-22 10:12:12 +0300

    fix comments

--- internal/app/config/compose.go
+++ internal/app/config/compose.go
@@ -1,10 +1,10 @@
 package config
 
-// CfgFunc - конфигурационная функция.
+// CfgFunc - конфигурационная функция для Compose.
 // Принимает на вход текущую конфигурацию и возвращает новую конфигурацию.
 type CfgFunc func(*Config) (*Config, error)
 
-// Compose - объединяет набор конфигурационных функций в одну
+// Compose - объединяет набор конфигурационных функций в итоговую конфигурацию
 func Compose(funcs ...CfgFunc) (*Config, error) {
 	var err error
 	cfg := &Config{}
